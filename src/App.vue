e2b766ef7631bad1382a12dd8627a4b239e87357
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-06-13 16:41:31 +0200

    moved code for calculator from app.vue to calculator component, started to work on calculator styles

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,50 @@
+<script setup>
+import Calculator from './components/CalculatorComp.vue';
+
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
 <template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-  <div class="calculatorContainer">
-  <div class="about">
-    <h1>This is a calculator page!!!</h1>
-  </div>
+    <!-- <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div> -->
+  </header>
 
-  <div class="calculatorScreen">
-    <p id="userInput">0</p>
-  </div>
-
-  <div class="row1">
-      <div class="numberButton7">7</div>
-      <div class="numberButton8">8</div>
-      <div class="numberButton9">9</div>
-      <div class="button%">%</div>
-  </div>
-  
-  <div class="row2">
-      <div class="numberButton4">4</div>
-      <div class="numberButton5">5</div>
-      <div class="numberButton6">6</div>
-      <div class="buttonX">x</div>
-  </div>
-
-  <div class="row3">
-      <div class="numberButton1">1</div>
-      <div class="numberButton2">2</div>
-      <div class="numberButton3">3</div>
-      <div class="number-">-</div>
-  </div>
-
-  <div class="row4">
-      <div class="buttonEmpty"></div>
-      <div class="button0">0</div>
-      <div class="button.">.</div>
-      <div class="button+">+</div>
-  </div>
-
-  <div class="row5">
-    <div class="buttonCE">CE</div>
-    <div class="buttonequal">=</div>
-  </div>
-</div>
+  <main>
+    <!-- <TheWelcome /> -->
+     <Calculator />
+  </main>
 </template>
 
 <style scoped>
-
-.calculatorContainer{
-
-background-color: rgb(44, 48, 81);
-
+/* header {
+  line-height: 1.5;
 }
-  .row1, .row2, .row3, .row4, .row5, .calculatorScreen{
-      display: flex;
-      align-items:center;
-      justify-content: center;
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+} */
 </style>
-
-<script setup>
-
-/// The backgroundColor of each button is: rgb(55, 60, 103);
-</script>
---
+++ src/components/CalculatorComp.vue
@@ -0,0 +1,74 @@
+<template>
+
+  <div class="calculatorContainer">
+    <div class="about">
+      <h1>This is a calculator page!!!</h1>
+    </div>
+  
+    <div class="calculatorScreen">
+      <p id="userInput">0</p>
+    </div>
+  
+    <div class="keyboard">
+
+      <div class="number">7</div>
+      <div class="number">8</div>
+      <div class="number">9</div>
+      <div class="operator">/</div>
+
+      <div class="number">4</div>
+      <div class="number">5</div>
+      <div class="number">6</div>
+      <div class="operator">×</div>
+
+      <div class="number">1</div>
+      <div class="number">2</div>
+      <div class="number">3</div>
+      <div class="operator">-</div>
+
+      <div class="number-empty"></div>
+      <div class="number">0</div>
+      <div class="number">.</div>
+      <div class="operator">+</div>
+
+    </div>
+      
+    <div class="row5">
+      <div class="buttonCE">CE</div>
+      <div class="buttonequal">=</div>
+    </div>
+  </div>
+  </template>
+  
+<style scoped lang="scss">
+
+.calculatorContainer {
+  background-color: rgb(44, 48, 81);
+  padding: 15px;
+}
+
+.calculatorScreen {
+  border: 1px solid red;
+}
+
+.keyboard {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+
+    div {
+      background-color: #373C67;
+      border-radius: 16px;
+      padding-block: 10px;
+      padding-inline: 21px;
+      text-align: center;
+      color: #E8E8EC;
+      font-size: 20px;
+    }
+}
+</style>
+
+<script setup>
+
+/// The backgroundColor of each button is: rgb(55, 60, 103);
+</script>
