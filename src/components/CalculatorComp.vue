89040d6563612a042990c28f0cfee9f12208e9a6
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-06-14 09:41:13 +0200

    styled calculator

--- src/components/CalculatorComp.vue
+++ src/components/CalculatorComp.vue
@@ -1,9 +1,10 @@
 <template>
 
+  <div class="heading">
+    <h1>Calculator</h1>
+  </div>
+
   <div class="calculatorContainer">
-    <div class="about">
-      <h1>This is a calculator page!!!</h1>
-    </div>
   
     <div class="calculatorScreen">
       <p id="userInput">0</p>
@@ -11,31 +12,31 @@
   
     <div class="keyboard">
 
-      <div class="number">7</div>
-      <div class="number">8</div>
-      <div class="number">9</div>
-      <div class="operator">/</div>
+      <div class="key number">7</div>
+      <div class="key number">8</div>
+      <div class="key number">9</div>
+      <div class="key operator">/</div>
 
-      <div class="number">4</div>
-      <div class="number">5</div>
-      <div class="number">6</div>
-      <div class="operator">×</div>
+      <div class="key number">4</div>
+      <div class="key number">5</div>
+      <div class="key number">6</div>
+      <div class="key operator">×</div>
 
-      <div class="number">1</div>
-      <div class="number">2</div>
-      <div class="number">3</div>
-      <div class="operator">-</div>
+      <div class="key number">1</div>
+      <div class="key number">2</div>
+      <div class="key number">3</div>
+      <div class="key operator">-</div>
 
-      <div class="number-empty"></div>
-      <div class="number">0</div>
-      <div class="number">.</div>
-      <div class="operator">+</div>
+      <div class="key number number-empty"></div>
+      <div class="key number">0</div>
+      <div class="key number">.</div>
+      <div class="key operator">+</div>
 
     </div>
       
-    <div class="row5">
-      <div class="buttonCE">CE</div>
-      <div class="buttonequal">=</div>
+    <div class="controls">
+      <div class="key buttonCE">CE</div>
+      <div class="key buttonequal">=</div>
     </div>
   </div>
   </template>
@@ -43,27 +44,52 @@
 <style scoped lang="scss">
 
 .calculatorContainer {
+  max-width: 310px;
   background-color: rgb(44, 48, 81);
   padding: 15px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 
 .calculatorScreen {
-  border: 1px solid red;
+  background-color: #373C67;
+  padding: 20px 15px;
+  border-radius: 16px;
+  text-align: right;
+  font-size: 20px;
 }
 
 .keyboard {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 10px;
+    .operator {
+      color: #EB8A30;
+    }
+}
 
-    div {
+.key {
+  background-color: #373C67;
+  border-radius: 16px;
+  padding-block: 10px;
+  padding-inline: 21px;
+  text-align: center;
+  color: #E8E8EC;
+  font-size: 20px;
+  }
+
+.controls {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+    .buttonCE {
       background-color: #373C67;
-      border-radius: 16px;
-      padding-block: 10px;
-      padding-inline: 21px;
-      text-align: center;
-      color: #E8E8EC;
-      font-size: 20px;
+      color: #1B54E9;
+    }
+    .buttonequal {
+      background-color: #1B54E9
     }
 }
 </style>
