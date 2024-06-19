f60d7a36abc31da1259eee7c9d8f98ef213797e7
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-06-19 10:11:03 +0200

    adding functions to our calculator

--- src/components/CalculatorComp.vue
+++ src/components/CalculatorComp.vue
@@ -7,36 +7,37 @@
   <div class="calculatorContainer">
   
     <div class="calculatorScreen">
-      <p id="userInput">0</p>
+      <p>{{ userInput }}</p>
     </div>
   
     <div class="keyboard">
 
-      <div class="key number">7</div>
-      <div class="key number">8</div>
-      <div class="key number">9</div>
-      <div class="key operator">/</div>
+      <div class="key number" @click="clickingNumbers(7)">7</div>
+      <div class="key number" @click="clickingNumbers(8)"> 8 </div>
+      <div class="key number" @click="clickingNumbers(9)">9</div>
+      <div class="key operator" @click="clickingOperators('/')">/</div>
 
-      <div class="key number">4</div>
-      <div class="key number">5</div>
-      <div class="key number">6</div>
-      <div class="key operator">×</div>
+      <div class="key number" @click="clickingNumbers(4)">4</div>
+      <div class="key number" @click="clickingNumbers(5)">5</div>
+      <div class="key number" @click="clickingNumbers(6)">6</div>
+      <div class="key operator" @click="clickingOperators('*')">×</div>
 
-      <div class="key number">1</div>
-      <div class="key number">2</div>
-      <div class="key number">3</div>
-      <div class="key operator">-</div>
+      <div class="key number" @click="clickingNumbers('1')">1</div>
+      <div class="key number" @click="clickingNumbers('2')">2</div>
+      <div class="key number" @click="clickingNumbers('3')">3</div>
+      <div class="key operator" @click="clickingOperators('-')">-</div>
+
 
       <div class="key number number-empty"></div>
-      <div class="key number">0</div>
-      <div class="key number">.</div>
-      <div class="key operator">+</div>
+      <div class="key number" @click="clickingNumbers('0')">0</div>
+      <div class="key number" @click="clickingNumbers('.')">.</div>
+      <div class="key operator" @click="clickingOperators('+')">+</div>
 
     </div>
       
     <div class="controls">
-      <div class="key buttonCE">CE</div>
-      <div class="key buttonequal">=</div>
+      <div class="key buttonCE" @click="ceScreen('ce')">CE</div>
+      <div class="key buttonequal" @click="makingEqual('=')">=</div>
     </div>
   </div>
   </template>
@@ -95,16 +96,65 @@
 </style>
 
 <script setup>
+import { ref } from 'vue';
 
-const numbers = document.querySelectorAll('.number');
-const calculatorScreen= document.getElementById('userInput');
 
-    numbers.forEach(number => {
-      number.addEventListener(onclick, ()=>{
-        calculatorScreen.innerHTML= `number.textContent`;
-        console.log('click');
-      })
-    });
+let userInput= ref('0');
 
-     log
+
+//Make a function for the numbers
+
+const clickingNumbers = (num) => {
+
+
+  if(userInput.value==='0'){
+    userInput.value = num;
+  } else{
+    userInput.value += num;
+  }
+};
+
+//Make a function for our operators
+
+const clickingOperators = (op) => {
+  if(userInput.value === '0'){
+    userInput.value = '0';
+  }else{
+    userInput.value += op
+  }
+};
+
+//Equal function, wich make my operations
+
+const makingEqual = (eq) => {
+
+  try{
+    userInput.value= eval(userInput.value)
+  } catch(error){
+    userInput.value= 'Error';
+  }
+
+};
+
+//CE function, wich delete my userInput.value content;
+
+const ceScreen = (ce) => {
+ userInput.value= '0'
+};
+
+
+
+
+
+
+
+///write a function wich regulate the operators
+
+
+
+
+
 </script>
+
+
+
