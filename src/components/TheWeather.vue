ec949082b97ec244e01368fc0b5240395b1ff594
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-06-21 12:51:38 +0200

    fixed icons display issue

--- src/components/TheWeather.vue
+++ src/components/TheWeather.vue
@@ -10,8 +10,9 @@ import defaultIcon from '@/assets/default.png'
 
 const city = ref('');
 const temperature = ref('');
-const icon = ref('');
+const icon = ref(defaultIcon);
 const stateSky = ref('');
+
 
 
 const fetchWeather = async () => {
@@ -19,36 +20,32 @@ const fetchWeather = async () => {
         let uri = 'https://www.el-tiempo.net/api/json/v2/provincias/33/municipios/33024';
         const response = await fetch(uri);
         let data = await response.json();
-        let sky = stateSky.value
+        // let sky = stateSky.value
         city.value = data.municipio.NOMBRE;
         temperature.value = data.temperatura_actual;
-        sky = data.stateSky.description.toLowerCase();
+        stateSky.value = data.stateSky.description.toLowerCase();
         console.log(data);
 
-        if (sky.includes('nuboso') || sky.includes('cubierto')) {
-            icon.value = cloudyIcon;
-        }
-
-        else if (sky.includes('despejado')){
-            icon.value= clearIcon;
-        }
-
-        else if(sky.includes('humedo')){
-            icon.value= wetIcon;
-        }
-        else if(sky.includes('lluvia')){
-            icon.value= rainIcon;
-        } 
-
-        else if(sky.includes('tormenta')){
+        if(stateSky.value.includes('tormenta')){
             icon.value= stormIcon;
         }
-        else if(sky.includes('nubes altas')){
-            icon.value= hightCloudIcon;
-        }else{
-            icon.value= defaultIcon;
+        else if(stateSky.value.includes('lluvia')){
+            icon.value= rainIcon;
+        } 
+        else if (stateSky.value.includes('nuboso') || stateSky.value.includes('cubierto')) {
+            icon.value = cloudyIcon;
         }
-        
+        else if (stateSky.value.includes('despejado')){
+            icon.value= clearIcon;
+        }
+        else if(stateSky.value.includes('humedo')){
+            icon.value= wetIcon;
+        }
+        else if(stateSky.value.includes('nubes altas')){
+            icon.value= hightCloudIcon;
+        } else {
+            icon.value = defaultIcon;
+        }
         
     }
     catch(error) {
@@ -75,5 +72,7 @@ onMounted(fetchWeather);
 .weather {
     background-color: #2C3051;
     padding: 20px;
+    max-width: 310px;
+    border-radius: 20px;
 }
 </style>
